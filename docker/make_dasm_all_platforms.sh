a911a4902b2abf2d52fc57630b137c953d603d10
dion.olsthoorn <Dion.Olsthoorn@wolterskluwer.com>
2019-09-03 22:32:56 +0200

    change: make_dasm_all_platforms.sh moves the platform-specific dasm binaries into the /bin folder

--- docker/make_dasm_all_platforms.sh
+++ docker/make_dasm_all_platforms.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 # builds dasm for all three supported platforms (linux, windows, macOS/darwin)
 
-#cd ~
-#rm -rf dasm
-#git clone --single-branch --branch master https://github.com/dasm-assembler/dasm.git
-#cd dasm
 cd ..
+mkdir -p bin
+cd src
 
 #step 1: build dasm for Linux x86 32-bit
 export CFLAGS=' -m32'
 export LDFLAGS=' -m32 -L/usr/lib32'
 make
-cp src/dasm ~/dasm.Linux.x86
+cp dasm ../bin/dasm.Linux.x86
 make clean
 
 #step 2: build dasm for macOS x86 32-bit
@@ -19,7 +17,7 @@ export CC=i686-w64-mingw32-gcc
 export CFLAGS=' -m32'
 export LDFLAGS=' -m32 -L/usr/lib32'
 make
-cp src/dasm ~/dasm.exe
+cp dasm ../bin/dasm.exe
 make clean
 
 #step 3: build dasm for macOS x86 32-bit
@@ -28,7 +26,7 @@ export CC=i686-apple-darwin10-gcc
 export CFLAGS=' -m32 -arch i386 '
 export LDFLAGS='-m32 -L/usr/i686-apple-darwin10/lib -arch i386'
 make
-cp src/dasm ~/dasm.Darwin.x86
+cp dasm ../bin/dasm.Darwin.x86
 make clean
 
 #
