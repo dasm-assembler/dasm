44c1804ed4b8c629b88fed3c3bfe1cfc9d4bd4b4
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2019-10-13 17:57:05 +0200

    modified build-script for all target platforms to include 64-bit versions for Linux and macOS

--- docker/make_dasm_all_platforms.sh
+++ docker/make_dasm_all_platforms.sh
@@ -2,31 +2,42 @@
 # this script builds platform-specific dasm binaries for all three supported OSs (Linux, Windows and macOS)
 
 cd ..
-mkdir -p bin
+mkdir -p bin/32bit
+mkdir -p bin/64bit
 cd src
 
-#step 1: build dasm for Linux x86 32-bit
+#step 1: build dasm for Linux 32-bit and 64-bit
+make
+cp dasm ../bin/64bit/dasm.Linux
+make clean
 export CFLAGS=' -m32'
 export LDFLAGS=' -m32 -L/usr/lib32'
 make
-cp dasm ../bin/dasm.Linux.x86
+cp dasm ../bin/32bit/dasm.Linux
 make clean
 
-#step 2: build dasm for Windows x86 32-bit
+#step 2: build dasm for Windows 32-bit
 export CC=i686-w64-mingw32-gcc
 export CFLAGS=' -m32'
 export LDFLAGS=' -m32 -L/usr/lib32'
 make
-cp dasm ../bin/dasm.exe
+cp dasm ../bin/32bit/dasm.exe
 make clean
+#TODO: fix issues when compiling for win64 using x86_64-w64-mingw32-gcc
+#      the compiler warnings have to do with incorrect size of longs
 
-#step 3: build dasm for macOS x86 32-bit
+#step 3: build dasm for macOS 32-bit and 64-bit
 export LD=$CC
 export CC=i686-apple-darwin10-gcc
-export CFLAGS=' -m32 -arch i386 '
-export LDFLAGS='-m32 -L/usr/i686-apple-darwin10/lib -arch i386'
+export CFLAGS=' -m64'
+export LDFLAGS=' -m64 -L/usr/i686-apple-darwin10/lib'
 make
-cp dasm ../bin/dasm.Darwin.x86
+cp dasm ../bin/64bit/dasm.macOS
+make clean
+export CFLAGS=' -m32 -arch i386 '
+export LDFLAGS=' -m32 -L/usr/i686-apple-darwin10/lib -arch i386'
+make
+cp dasm ../bin/32bit/dasm.macOS
 make clean
 
 #
@@ -34,4 +45,3 @@ unset LD
 unset CC
 unset CFLAGS
 unset LDFLAGS
-
