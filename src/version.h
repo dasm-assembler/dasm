f63070b6b4cd2eaca42d74a38fb020c7a818afd3
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2020-02-19 23:02:03 +0100

    moved master version to 2.20.14-SNAPSHOT

--- src/version.h
+++ src/version.h
@@ -31,7 +31,7 @@
 /**
  * @brief Macro to provide current DASM release as a string.
  */
-#define DASM_RELEASE "2.20.13"
+#define DASM_RELEASE "2.20.14-SNAPSHOT"
 
 /**
  * @brief Macro to provide current DASM headline, including
