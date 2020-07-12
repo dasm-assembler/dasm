6a2f1ad32931d4f878375342f35c7811c68b7169
github@pantzer.net <github@pantzer.net>
2020-07-13 01:37:01 +0200

    Copyright notice corrected

--- src/mne68908.c
+++ src/mne68908.c
@@ -1,8 +1,29 @@
+/*
+    the DASM macro assembler (aka small systems cross assembler)
+
+    Copyright (c) 1988-2002 by Matthew Dillon.
+    Copyright (c) 1995 by Olaf "Rhialto" Seibert.
+    Copyright (c) 2003-2008 by Andrew Davie.
+    Copyright (c) 2008 by Peter H. Froehlich.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 /*
- *  MNE68908.C
- *
- *  (c)Copyright 2003, Thomas Pantzer, All Rights Reserved.
+ *  MNE68908.C 
+ *     based on MNE68705.C	 Thomas Pantzer, 2003-06-18
  */
 
 #include "asm.h"
