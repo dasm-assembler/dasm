a45cf7bbd9d18c560dc11161866a3faaf1cf88b6
github@pantzer.net <github@pantzer.net>
2020-07-15 03:21:15 +0200

    Complete overhaul of run_test.sh script. Fix for Issue #61

---
+++ test/selftest/0_no_assembly.asm
@@ -0,0 +1,8 @@
+
+
+	.processor fantasy
+	.org 0
+
+;
+; fake processor in order to abort assembly, testing the test-enginge for aborted assembly
+;
---
+++ test/selftest/2_hexref_missing.asm
@@ -0,0 +1,9 @@
+
+
+	.processor 6502
+	.org 0
+
+	DC.w 1234
+;
+; should create a file, testing the test-enginge for missing hex reference
+;
